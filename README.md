4f943621790c1682359ce279aecb0474bdfb906a
Astroyo <alifah.17273@gmail.com>
2024-08-31 14:00:51 -0700

    added localstorage

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TODO LIST
-A simple todo list website.
+
+A stylish yet simple todo list that remembers your todos and allows you to easily add and remove them.
+
+### Behind the scenes
+One of my earliest projects of when I was learning web development. A neglected one to which I have recently added local storage so it has persistent storage. Pretty basic but yeah.
--- script.js
+++ script.js
@@ -1,28 +1,30 @@
+// Fix this shit code and make it humane
 
-let input = document.getElementById("input");
-let button = document.getElementById("button");
-let list = document.getElementById("list");
-let todos = document.querySelectorAll("li")
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const list = document.getElementById("list");
+const todos = document.querySelectorAll("li")
 
-let toggleToDo = (tag) => {
+const toggleToDo = tag => {
     tag.addEventListener("click", () => {
         tag.classList.toggle("done");
     })
 }
 
-let removeToDo = (container, tag) => {
+const removeToDo = (container, tag) => {
     tag.addEventListener("click", () => {
+        const value = container[0].firstChild.nodeValue
+        todosStorage.removeText(value)
         container[0].remove()
         container[1].remove()
     })
 }
 
-let newToDo = () => {
+const newToDo = value => {
     let div = document.createElement("div");
 
-
     let li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
+    li.appendChild(document.createTextNode(value));
     div.appendChild(li);
     toggleToDo(li)
     input.value = "";
@@ -43,14 +45,54 @@ let newToDo = () => {
 
 button.addEventListener("click", () => {
     if (!(input.value.length > 0)) { return; }
-    newToDo()
+    todosStorage.addText(input.value)
+    newToDo(input.value)
 });
 
 
 input.addEventListener("keypress", event => {
     if (input.value.length > 0 && event.key === "Enter") {
-        newToDo()
+        todosStorage.addText(input.value)
+        newToDo(input.value)
     }
 });
 
 
+
+
+class TodosStorage {
+    constructor(storageKey = 'todos') {
+        this.storageKey = storageKey;
+    }
+
+    // Get all todos from storage
+    getAllTexts() {
+        const storedData = localStorage.getItem(this.storageKey);
+        return storedData ? JSON.parse(storedData) : [];
+    }
+
+    // Add a new todos to storage
+    addText(todo) {
+        const todos = this.getAllTexts();
+        todos.push(todo);
+        localStorage.setItem(this.storageKey, JSON.stringify(todos));
+    }
+
+    // Remove a specific todos from storage
+    removeText(todo) {
+        let todos = this.getAllTexts();
+        todos = todos.filter(storedName => storedName !== todo);
+        localStorage.setItem(this.storageKey, JSON.stringify(todos));
+    }
+
+    // Remove all todos (clear storage)
+    removeAllTexts() {
+        localStorage.removeItem(this.storageKey);
+    }
+}
+
+const todosStorage = new TodosStorage();
+
+document.addEventListener('DOMContentLoaded', () => {
+    todosStorage.getAllTexts().forEach(todo => newToDo(todo));
+})
